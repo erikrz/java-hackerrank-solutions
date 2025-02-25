erikrz/hackerrank-solutions

Add BundlePricingChallenge

src/main/java/com/erikrz/hackerrank/solutions/challenges/simplebundleprincing/CartBundlePricing.java
@@ -1,0 +1,52 @@
+package com.erikrz.hackerrank.solutions.challenges.simplebundleprincing;
+
+import com.erikrz.hackerrank.solutions.challenges.simplebundleprincing.dto.Bundle;
+import com.erikrz.hackerrank.solutions.challenges.simplebundleprincing.dto.Cart;
+import com.erikrz.hackerrank.solutions.challenges.simplebundleprincing.dto.Item;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class CartBundlePricing {
+
+    public Cart applyBundlePricing(Cart cart, List<Bundle> bundles) {
+        System.out.println("Initial cart price: " + cart.getTotalPrice());
+        var validBundles = getPotentialBundles(cart, bundles);
+        if (validBundles.isEmpty()) {
+            return cart;
+        }
+        var cartWithBundlePricing= validBundles.stream()
+                .flatMap(bundle -> applyBundle(cart, bundle).stream())
+                .map(cartIteration -> applyBundlePricing(cartIteration, validBundles))
+                .min(Comparator.comparing(Cart::getTotalPrice))
+                .orElse(cart);
+        System.out.println("BundlePricing cart price: " + cartWithBundlePricing.getTotalPrice());
+        return cartWithBundlePricing;
+    }
+
+    private List<Bundle> getPotentialBundles(Cart cart, List<Bundle> bundles) {
+        return bundles.stream().filter(
+                bundle -> cart.items().containsAll(bundle.items())
+        ).toList();
+    }
+
+    private Optional<Cart> applyBundle(Cart cart, Bundle bundle) {
+        var newCartItems = new ArrayList<>(cart.items());
+        var removedItems = bundle.items().stream()
+                .map(newCartItems::remove)
+                .filter(removed -> removed)
+                .count();
+        if (removedItems != bundle.items().size()) {
+            return Optional.empty();
+        }
+        newCartItems.add(bundleToItem(bundle));
+        return Optional.of(new Cart(List.copyOf(newCartItems)));
+    }
+
+    private Item bundleToItem(Bundle bundle) {
+        return new Item(bundle.name(), bundle.price());
+    }
+
+}

src/main/java/com/erikrz/hackerrank/solutions/challenges/simplebundleprincing/dto/Bundle.java
@@ -1,0 +1,7 @@
+package com.erikrz.hackerrank.solutions.challenges.simplebundleprincing.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record Bundle (String name, List<Item> items, BigDecimal price){
+}

src/main/java/com/erikrz/hackerrank/solutions/challenges/simplebundleprincing/dto/Cart.java
@@ -1,0 +1,13 @@
+package com.erikrz.hackerrank.solutions.challenges.simplebundleprincing.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record Cart(List<Item> items) {
+
+    public BigDecimal getTotalPrice() {
+        return items.stream()
+                .map(Item::price)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}

src/main/java/com/erikrz/hackerrank/solutions/challenges/simplebundleprincing/dto/Item.java
@@ -1,0 +1,5 @@
+package com.erikrz.hackerrank.solutions.challenges.simplebundleprincing.dto;
+
+import java.math.BigDecimal;
+
+public record Item(String name, BigDecimal price) { }

src/test/java/com/erikrz/hackerrank/solutions/challenges/simplebundleprincing/CartBundlePricingTest.java
@@ -1,0 +1,78 @@
+package com.erikrz.hackerrank.solutions.challenges.simplebundleprincing;
+
+
+import com.erikrz.hackerrank.solutions.challenges.simplebundleprincing.dto.Bundle;
+import com.erikrz.hackerrank.solutions.challenges.simplebundleprincing.dto.Cart;
+import com.erikrz.hackerrank.solutions.challenges.simplebundleprincing.dto.Item;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CartBundlePricingTest {
+
+    private final Item mango = new Item("Mango", new BigDecimal("2.50"));
+    private final Item apple = new Item("Apple", new BigDecimal("1.50"));
+    private final Item bread = new Item("Bread", new BigDecimal("1.00"));
+    private final Item banana = new Item("Banana", new BigDecimal("1.00"));
+    private final Item margarine = new Item("Margarine", new BigDecimal("1.00"));
+    private final Item orange = new Item("Orange", new BigDecimal("2.00"));
+    private final Item grape = new Item("Grape", new BigDecimal("3.00"));
+
+    private final Bundle mangoAppleBundle = new Bundle("mango-apple Bundle", List.of(mango, apple), new BigDecimal("3.15"));
+    private final Bundle breadMargarineBundle = new Bundle("bread-margarine Bundle", List.of(bread, margarine), new BigDecimal("1.80"));
+    private final Bundle grapeMangoBundle = new Bundle("grape-mango Bundle", List.of(grape, mango), new BigDecimal("5.00"));
+    private final Bundle appleAppleBundle = new Bundle("apple-apple Bundle", List.of(apple, apple), new BigDecimal("2.15"));
+
+    private final List<Bundle> allBundles = List.of(mangoAppleBundle, breadMargarineBundle, grapeMangoBundle, appleAppleBundle);
+
+    private final CartBundlePricing cartBundlePricing = new CartBundlePricing();
+
+    @Test
+    void applyBundlePricingToEmptyCart() {
+        var cart = new Cart(emptyList());
+        var expectedCart = new Cart(emptyList());
+        var result = cartBundlePricing.applyBundlePricing(cart, allBundles);
+        assertThat(result).isEqualTo(expectedCart);
+    }
+
+    @Test
+    void applyBundlePricingToCartWithNoAvailableBundles() {
+        var cart = new Cart(List.of(mango, banana, orange));
+        var expectedCart = new Cart(List.of(mango, banana, orange));
+        var result = cartBundlePricing.applyBundlePricing(cart, allBundles);
+        assertThat(result).isEqualTo(expectedCart);
+    }
+
+    @Test
+    void applyBundlePricingToCartWithSingleAvailableBundle() {
+        var cart = new Cart(List.of(mango, apple));
+        var expectedCart = new Cart(List.of(toItem(mangoAppleBundle)));
+        var result = cartBundlePricing.applyBundlePricing(cart, allBundles);
+        assertThat(result).isEqualTo(expectedCart);
+    }
+
+    @Test
+    void applyBundlePricingToCartWith2TimesSingleAvailableBundle() {
+        var cart = new Cart(List.of(mango, mango, apple, apple));
+        var expectedCart = new Cart(List.of(toItem(mangoAppleBundle), toItem(mangoAppleBundle)));
+        var result = cartBundlePricing.applyBundlePricing(cart, allBundles);
+        assertThat(result).isEqualTo(expectedCart);
+    }
+
+    @Test
+    void applyBundlePricingToCartWithCompetingBundles() {
+        var cart = new Cart(List.of(mango, apple, grape));
+        var expectedCart = new Cart(List.of(grape, toItem(mangoAppleBundle)));
+        var result = cartBundlePricing.applyBundlePricing(cart, allBundles);
+        assertThat(result).isEqualTo(expectedCart);
+    }
+
+    private Item toItem(Bundle bundle) {
+        return new Item(bundle.name(), bundle.price());
+    }
+
+}

erikrz/java-hackerrank-solutions

src/main/java/com/erikrz/hackerrank/solutions/challenges/simplebundlepricing/CartBundlePricing.java
@@ -1,0 +1,52 @@
+package com.erikrz.hackerrank.solutions.challenges.simplebundlepricing;
+
+import com.erikrz.hackerrank.solutions.challenges.simplebundlepricing.dto.Bundle;
+import com.erikrz.hackerrank.solutions.challenges.simplebundlepricing.dto.Cart;
+import com.erikrz.hackerrank.solutions.challenges.simplebundlepricing.dto.Item;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class CartBundlePricing {
+
+    public Cart applyBundlePricing(Cart cart, List<Bundle> bundles) {
+        System.out.println("Initial cart price: " + cart.getTotalPrice());
+        var validBundles = getPotentialBundles(cart, bundles);
+        if (validBundles.isEmpty()) {
+            return cart;
+        }
+        var cartWithBundlePricing= validBundles.stream()
+                .flatMap(bundle -> applyBundle(cart, bundle).stream())
+                .map(cartIteration -> applyBundlePricing(cartIteration, validBundles))
+                .min(Comparator.comparing(Cart::getTotalPrice))
+                .orElse(cart);
+        System.out.println("BundlePricing cart price: " + cartWithBundlePricing.getTotalPrice());
+        return cartWithBundlePricing;
+    }
+
+    private List<Bundle> getPotentialBundles(Cart cart, List<Bundle> bundles) {
+        return bundles.stream().filter(
+                bundle -> cart.items().containsAll(bundle.items())
+        ).toList();
+    }
+
+    private Optional<Cart> applyBundle(Cart cart, Bundle bundle) {
+        var newCartItems = new ArrayList<>(cart.items());
+        var removedItems = bundle.items().stream()
+                .map(newCartItems::remove)
+                .filter(removed -> removed)
+                .count();
+        if (removedItems != bundle.items().size()) {
+            return Optional.empty();
+        }
+        newCartItems.add(bundleToItem(bundle));
+        return Optional.of(new Cart(List.copyOf(newCartItems)));
+    }
+
+    private Item bundleToItem(Bundle bundle) {
+        return new Item(bundle.name(), bundle.price());
+    }
+
+}

src/main/java/com/erikrz/hackerrank/solutions/challenges/simplebundlepricing/dto/Bundle.java
@@ -1,0 +1,7 @@
+package com.erikrz.hackerrank.solutions.challenges.simplebundlepricing.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record Bundle (String name, List<Item> items, BigDecimal price){
+}

src/main/java/com/erikrz/hackerrank/solutions/challenges/simplebundlepricing/dto/Cart.java
@@ -1,0 +1,13 @@
+package com.erikrz.hackerrank.solutions.challenges.simplebundlepricing.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record Cart(List<Item> items) {
+
+    public BigDecimal getTotalPrice() {
+        return items.stream()
+                .map(Item::price)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}

src/main/java/com/erikrz/hackerrank/solutions/challenges/simplebundlepricing/dto/Item.java
@@ -1,0 +1,5 @@
+package com.erikrz.hackerrank.solutions.challenges.simplebundlepricing.dto;
+
+import java.math.BigDecimal;
+
+public record Item(String name, BigDecimal price) { }

src/test/java/com/erikrz/hackerrank/solutions/challenges/simplebundlepricing/CartBundlePricingTest.java
@@ -1,0 +1,88 @@
+package com.erikrz.hackerrank.solutions.challenges.simplebundlepricing;
+
+
+import com.erikrz.hackerrank.solutions.challenges.simplebundlepricing.dto.Bundle;
+import com.erikrz.hackerrank.solutions.challenges.simplebundlepricing.dto.Cart;
+import com.erikrz.hackerrank.solutions.challenges.simplebundlepricing.dto.Item;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CartBundlePricingTest {
+
+    private final Item mango = new Item("Mango", new BigDecimal("2.50"));
+    private final Item apple = new Item("Apple", new BigDecimal("1.50"));
+    private final Item bread = new Item("Bread", new BigDecimal("1.00"));
+    private final Item banana = new Item("Banana", new BigDecimal("1.00"));
+    private final Item margarine = new Item("Margarine", new BigDecimal("1.00"));
+    private final Item orange = new Item("Orange", new BigDecimal("2.00"));
+    private final Item grape = new Item("Grape", new BigDecimal("3.00"));
+
+    private final Bundle mangoAppleBundle = new Bundle("mango-apple Bundle", List.of(mango, apple), new BigDecimal("3.15"));
+    private final Bundle breadMargarineBundle = new Bundle("bread-margarine Bundle", List.of(bread, margarine), new BigDecimal("1.80"));
+    private final Bundle grapeMangoBundle = new Bundle("grape-mango Bundle", List.of(grape, mango), new BigDecimal("5.00"));
+    private final Bundle appleAppleBundle = new Bundle("apple-apple Bundle", List.of(apple, apple), new BigDecimal("2.15"));
+
+    private final List<Bundle> allBundles = List.of(mangoAppleBundle, breadMargarineBundle, grapeMangoBundle, appleAppleBundle);
+
+    private final CartBundlePricing cartBundlePricing = new CartBundlePricing();
+
+    @Test
+    void applyBundlePricingToEmptyCart() {
+        var cart = new Cart(emptyList());
+        var expectedCart = new Cart(emptyList());
+        
+        var result = cartBundlePricing.applyBundlePricing(cart, allBundles);
+        
+        assertThat(result).isEqualTo(expectedCart);
+    }
+
+    @Test
+    void applyBundlePricingToCartWithNoAvailableBundles() {
+        var cart = new Cart(List.of(mango, banana, orange));
+        var expectedCart = new Cart(List.of(mango, banana, orange));
+        
+        var result = cartBundlePricing.applyBundlePricing(cart, allBundles);
+        
+        assertThat(result).isEqualTo(expectedCart);
+    }
+
+    @Test
+    void applyBundlePricingToCartWithSingleAvailableBundle() {
+        var cart = new Cart(List.of(mango, apple));
+        var expectedCart = new Cart(List.of(toItem(mangoAppleBundle)));
+        
+        var result = cartBundlePricing.applyBundlePricing(cart, allBundles);
+        
+        assertThat(result).isEqualTo(expectedCart);
+    }
+
+    @Test
+    void applyBundlePricingToCartWith2TimesSingleAvailableBundle() {
+        var cart = new Cart(List.of(mango, mango, apple, apple));
+        var expectedCart = new Cart(List.of(toItem(mangoAppleBundle), toItem(mangoAppleBundle)));
+        
+        var result = cartBundlePricing.applyBundlePricing(cart, allBundles);
+        
+        assertThat(result).isEqualTo(expectedCart);
+    }
+
+    @Test
+    void applyBundlePricingToCartWithCompetingBundles() {
+        var cart = new Cart(List.of(mango, apple, grape));
+        var expectedCart = new Cart(List.of(grape, toItem(mangoAppleBundle)));
+        
+        var result = cartBundlePricing.applyBundlePricing(cart, allBundles);
+        
+        assertThat(result).isEqualTo(expectedCart);
+    }
+
+    private Item toItem(Bundle bundle) {
+        return new Item(bundle.name(), bundle.price());
+    }
+
+}