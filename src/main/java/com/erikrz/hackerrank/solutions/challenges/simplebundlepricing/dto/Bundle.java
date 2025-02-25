1527820faf0ca7cdf311ac4560807117ae450816
Luis Erik Ríos Hernández <erik.rz@live.com>
2025-02-25 17:00:11 -0600

    Revert "Add BundlePricingChallenge"
    This reverts commit ee1037f906568ac88db194e382b2459d4001d6e6.

--- src/main/java/com/erikrz/hackerrank/solutions/challenges/simplebundlepricing/dto/Bundle.java
+++
@@ -1,7 +0,0 @@
-package com.erikrz.hackerrank.solutions.challenges.simplebundlepricing.dto;
-
-import java.math.BigDecimal;
-import java.util.List;
-
-public record Bundle (String name, List<Item> items, BigDecimal price){
-}
--- src/main/java/com/erikrz/hackerrank/solutions/challenges/simplebundlepricing/dto/Item.java
+++
@@ -1,5 +0,0 @@
-package com.erikrz.hackerrank.solutions.challenges.simplebundlepricing.dto;
-
-import java.math.BigDecimal;
-
-public record Item(String name, BigDecimal price) { }
